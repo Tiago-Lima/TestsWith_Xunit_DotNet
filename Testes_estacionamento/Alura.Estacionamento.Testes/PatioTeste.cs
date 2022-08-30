5ed5125e5ea3f40ff1508f3227987192c1194737
Tiago-Lima <tiago.santoslima91@gmail.com>
2022-08-29 21:04:30 -0300

    Adicionando teste de faturamento da classse patio

---
+++ Testes_estacionamento/Alura.Estacionamento.Testes/PatioTeste.cs
@@ -0,0 +1,62 @@
+﻿using Alura.Estacionamento.Alura.Estacionamento.Modelos;
+using Alura.Estacionamento.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Alura.Estacionamento.Testes
+{
+    public class PatioTeste
+    {
+        [Fact]
+
+        void ValidaFaturamento()
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo("Tiago Lima");
+            veiculo.Tipo = TipoVeiculo.Automovel;
+            veiculo.Cor = "Verde";
+            veiculo.Modelo = "Fusca";
+            veiculo.Placa = "asd-1989";
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            //Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            //Assert
+            Assert.Equal(2, faturamento);
+
+        }
+
+        [Theory]
+        [InlineData("Jorge Almeida", "FDE-1991", "Preto", "Palio")] //Anotação que permite passar vários valores para o mesmo teste
+        [InlineData("Lucas Almeida", "PDM-1991", "Prata", "Gol")]
+        [InlineData("Mauricio Almeida", "ADF-1991", "Azul", "Celta")]
+
+        public void ValidaFaturamentoComVariosVeiculos(string proprietario, string placa, string cor, string modelo)
+        {
+            // Arrange
+            var estacionamento = new Patio();
+            var veiculo = new Veiculo(proprietario);
+            veiculo.Cor = cor;
+            veiculo.Modelo = modelo;
+            veiculo.Placa = placa;
+
+            estacionamento.RegistrarEntradaVeiculo(veiculo);
+            estacionamento.RegistrarSaidaVeiculo(veiculo.Placa);
+
+            // Act
+            double faturamento = estacionamento.TotalFaturado();
+
+            // Acert
+            Assert.Equal(2, faturamento);
+
+        }
+    }
+}
